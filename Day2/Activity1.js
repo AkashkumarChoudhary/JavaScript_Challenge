73b812c9ccd95048d3c1ab981052936a141faf18
AkashkumarChoudhary <akcmumbai13579@gmail.com>
2024-07-17 23:32:32 +0530

    17

---
+++ Day2/Activity1.js
@@ -0,0 +1,12 @@
+function addNumber (a , b) {
+    return a + b;
+
+}
+
+const num1 = 8;
+const num2   = 4;
+
+
+const result = addNumber(num1 ,num2)
+
+console.log(`The sum of  ${num1} and ${num2} is ${result}  `);
